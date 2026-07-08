2212c64bf97bd6a199d47c2a8facc298d95d4c41
Alain <alahin66@hotmail.com>
2026-07-08 19:22:42 +0200

    Initial commit

--- components/analytics/Clarity.tsx
+++ components/analytics/Clarity.tsx
@@ -1,16 +1,14 @@
-"use client";
+import Script from "next/script";
 
-import { useEffect } from "react";
-import clarity from "@microsoft/clarity";
-
-export default function Clarity() {
-  useEffect(() => {
-    const projectId = process.env.NEXT_PUBLIC_CLARITY_ID;
-
-    if (projectId) {
-      clarity.init(projectId);
-    }
-  }, []);
-
-  return null;
-}
+<Script
+  id="ms-clarity"
+  strategy="afterInteractive"
+>
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+  `}
+</Script>
