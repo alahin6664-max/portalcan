5013543e6526dea4d149ccc5e2d428b441a55deb
Alain <alahin66@hotmail.com>
2026-07-08 19:26:58 +0200

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import Clarity from "@/components/analytics/Clarity";
+import Script from "next/script";
 
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
@@ -32,7 +32,18 @@ export default function RootLayout({
 
         
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
-        <Clarity />
+        <Script
+          id="ms-clarity"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+          `}
+        </Script>
       </body>
     </html>
   );
--- components/analytics/Clarity.tsx
+++ components/analytics/Clarity.tsx
@@ -1,14 +1,16 @@
-import Script from "next/script";
+"use client";
 
-<Script
-  id="ms-clarity"
-  strategy="afterInteractive"
->
-  {`
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
-  `}
-</Script>
+import { useEffect } from "react";
+import clarity from "@microsoft/clarity";
+
+export default function Clarity() {
+  useEffect(() => {
+    const projectId = process.env.NEXT_PUBLIC_CLARITY_ID;
+
+    if (projectId) {
+      clarity.init(projectId);
+    }
+  }, []);
+
+  return null;
+}
